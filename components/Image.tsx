5cad2497142c3ebd92ca41e6ffba393114c20324
なのくろ <nanocloudx@gmail.com>
2024-11-22 17:22:34 +0900

    update

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,12 +3,13 @@ import style from './Image.module.scss'
 type Props = {
   src: string
   alt?: string
+  height?: string
 }
 
-export function Image({src, alt}: Props) {
+export function Image({src, alt, height = '60mm'}: Props) {
   return (
     <div className={style.main}>
-      <img className={style.image} src={src}/>
+      <img className={style.image} style={{maxHeight: height}} src={src}/>
       {alt && <small className={style.caption}>{alt}</small>}
     </div>
   )
