82024f1dc926e326ff76387c6cbf8b02b2149c87
なのくろ <nanocloudx@gmail.com>
2024-11-26 01:52:53 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ export default function Page() {
   return (
     <div style={{margin: '5rem', padding: '5rem', textAlign: 'center', backgroundColor: '#fff'}}>
       <h1 style={{marginBottom: '1rem'}}>サイバーメタルチンピラ Viewer</h1>
-      <a href={'/2024'} style={{fontSize: '1.5rem'}}>2024</a>
+      <p>
+        <a href={'/2024'} style={{fontSize: '1.5rem'}}>2024</a>
+      </p>
+      <p>
+        <a href={'https://github.com/nanocloudx/cybermetalchimpira'} style={{fontSize: '1.5rem'}}>GitHub</a>
+      </p>
     </div>
   )
 }
