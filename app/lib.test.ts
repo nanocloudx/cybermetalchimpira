3b3c6e4e12a0833292275ef0b688ed521dd8064c
なのくろ <nanocloudx@gmail.com>
2024-11-28 17:37:04 +0900

    update

---
+++ app/lib.test.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest'
+import {getStartPage} from '@/app/lib'
+import {articleInfoList as list2024} from '@/app/2024/articleInfoList'
+
+test('入稿可能なページ数である', () => {
+  const lastPage = getStartPage('okuduke', list2024)
+  expect(lastPage % 4).toBe(0)
+})
---
+++ vitest.config.js
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default {
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './')
+    },
+  },
+}
