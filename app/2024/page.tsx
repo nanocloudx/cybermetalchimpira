c40c90d7965227ac29d632a266c83a1235583209
なのくろ <nanocloudx@gmail.com>
2024-12-09 15:20:47 +0900

    update

--- app/2024/page.tsx
+++ app/2024/page.tsx
@@ -1,13 +1,16 @@
 import {Navigation} from '@/components/Navigation'
 import {articleInfoList} from '@/app/2024/articleInfoList'
 import {PreviewHyoshiPage} from '@/components/PreviewHyoshiPage'
+import {BlankPage} from '@/components/BlankPage'
 
 export default function Page() {
   return (
     <div>
       <Navigation year={'2024'} articleInfoList={articleInfoList}/>
       <PreviewHyoshiPage src={'/2024/hyoshi/hyoshi1.png'}/>
+      <BlankPage />
       {articleInfoList.map((info) => info.component)}
+      <BlankPage />
       <PreviewHyoshiPage src={'/2024/hyoshi/hyoshi2.png'}/>
     </div>
   )
---
+++ components/BlankPage.tsx
@@ -0,0 +1,7 @@
+import style from './BlankPage.module.scss'
+
+export function BlankPage() {
+  return (
+    <div className={style.main} />
+  )
+}
